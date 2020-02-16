2ceaa31eb03cc5e16a7a92f9606b447617cb4994
Anton Danilov <anton2009danilov@yandex.ru>
2020-02-16 18:51:56 +0300

    задание 1.3

--- homework_5.sql
+++ homework_5.sql
@@ -1,3 +1,5 @@
+-- “Операторы, фильтрация, сортировка и ограничение”
+
 -- 1. Пусть в таблице users поля created_at и updated_at оказались незаполненными. Заполните их текущими датой и временем.
 
 	update users set updated_at = now(), created_at = now(); -- в этой таблице у меня нет полей с датами
@@ -27,3 +29,22 @@
 	-- select * from test;
 	-- update test set date = replace(date, '.', ',') where id = 1;
 	-- update test set date = str_to_date(replace(date, '.', ','), '%d, %m, %Y %T') where id = 1;
+	
+	
+	-- 3. В таблице складских запасов storehouses_products в поле value могут встречаться самые разные цифры:
+	--    0, если товар закончился и выше нуля, если на складе имеются запасы.
+	--    Необходимо отсортировать записи таким образом, чтобы они выводились в порядке увеличения значения value. Однако,
+	--    нулевые запасы должны выводиться в конце, после всех записей.
+	
+	create table storehouses_products(
+		id serial primary key,
+		name varchar(150),
+		value int
+	);
+	
+	insert into storehouses_products (`name`,`value`) values
+		('заяц',0), ('мишка',4), ('котик',0), ('собачка',23), ('мышка',1), ('лягушка',2), ('динозавр', 0);
+	
+	select `name`, `value` from storehouses_products order by 1/value desc;
+
+		
