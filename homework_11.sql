a93d486bd52798c809ac214f5a4731fbfe0008fc
Anton Danilov <anton2009danilov@yandex.ru>
2020-03-16 00:25:02 +0300

    Практическое задание по теме “NoSQL” Часть 3

--- homework_11.sql
+++ homework_11.sql
@@ -44,31 +44,62 @@ insert into products (name, description, price, catalog_id) values ('Logitech 10
 
 -- Создание коллекции ip-адресов:
 -- 
--- hset ips 170.0.1.1 1
--- hset ips 155.0.1.1 55
--- hset ips 111.0.1.3 33
--- 
+hset ips 170.0.1.1 1
+hset ips 155.0.1.1 55
+hset ips 111.0.1.3 33
+ 
 -- Увеличение числа посещений на единицу:
--- 
--- hincrby ips 170.0.1.1 1
+
+hincrby ips 170.0.1.1 1
 
 # 2. При помощи базы данных Redis решите задачу поиска имени пользователя по электронному адресу и наоборот,
 -- поиск электронного адреса пользователя по его имени.
 
 -- Создание коллекции пользователей
--- hset users alex1123 alxxx@mail.ru
--- hset users 'Юрий Фомин' yura2@inbox.ru
+hset users alex1123 alxxx@mail.ru
+hset users 'Юрий Фомин' yura2@inbox.ru
 
 
 -- Поиск адреса по имени пользователя
--- hget users alex1123
+hget users alex1123
 
 
 -- Создание коллекции электронных адресов
--- hset emails alxxx@mail.ru alex1123
--- hset emails yura2@inbox.ru 'Юрий Фомин'
+hset emails alxxx@mail.ru alex1123
+hset emails yura2@inbox.ru 'Юрий Фомин'
 
 -- Поиск имени пользователя по адресу
--- hget emails yura2@inbox.ru
+hget emails yura2@inbox.ru
 
 
+# 3. Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
+
+-- Создание коллекции catalogs
+db.catalogs.insert({id:1, name:'Процессоры'}
+db.catalogs.insert({id:2, name: 'Материнские платы'})
+db.catalogs.insert({id:3, name: 'Видеокарты'})
+
+-- Создание коллекции products
+db.products.insert({
+	id:1,
+	name:'Intel Core i3-8100',
+	description:'Процессор для настольных персональных компьютеров, основанных на платформе Intel.',
+	price: 7890.00,
+	catalog_id: 1
+})
+
+db.products.insert({
+	id:2,
+	name:'AMD FX-8320E',
+	description:'Процессор для настольных персональных компьютеров, основанных на платформе AMD.',
+	price: 4780.00,
+	catalog_id: 1
+})
+
+db.products.insert({
+	id:3,
+	name:'MSI B250M GAMING PRO',
+	description:'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX.',
+	price: 5060.00,
+	catalog_id: 2
+})
