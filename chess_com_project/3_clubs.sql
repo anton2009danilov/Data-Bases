6723a2e1d2f6c3813bf0a9f95ddf0ff9afe6cc4a
Anton Danilov <anton2009danilov@yandex.ru>
2020-03-20 00:12:38 +0300

    оптимизация Insert запросов

--- chess_com_project/3_clubs.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `clubs` (`id`, `creator_id`, `name`) VALUES ('1', '1', 'Quibusdam dolores est voluptas eius.');
-INSERT INTO `clubs` (`id`, `creator_id`, `name`) VALUES ('2', '2', 'Autem eligendi sed nihil nam.');
-INSERT INTO `clubs` (`id`, `creator_id`, `name`) VALUES ('3', '3', 'Labore temporibus consequatur aliquam non.');
-INSERT INTO `clubs` (`id`, `creator_id`, `name`) VALUES ('4', '4', 'Iure pariatur ratione et in minima et.');
-INSERT INTO `clubs` (`id`, `creator_id`, `name`) VALUES ('5', '5', 'Eveniet nisi maiores ea magni modi.');
-INSERT INTO `clubs` (`id`, `creator_id`, `name`) VALUES ('6', '6', 'Nulla sapiente totam incidunt dolores aut temporibus est repudiandae.');
-
-
